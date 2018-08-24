4a3d8f58faf9ad0d25553418af3d2b0cc3ae1720
Neo Morina <neomorina@gmail.com>
2018-08-24 10:43:09 +0200

    Fixed an error

--- src/Api/Image.php
+++ src/Api/Image.php
@@ -39,33 +39,9 @@ class Image
             } else {
                 header('HTTP/1.0 404 Not Found');
                 die();
-                    $etag = '"'.$user_data['account_id'].':"';
-                    header('Content-type: image/jpeg');
-                    header('ETag: '.$etag);
-                    // if etag parameter given in url, we can allow browser to cache picture via an Expires header
-                    // different url with different etag parameter will force a reload
-                    if (isset($_GET['etag'])) {
-                        Api\Session::cache_control(30 * 86400); // cache for 30 days
-                    }
-                    // if servers send a If-None-Match header, response with 304 Not Modified, if etag matches
-                    if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] === $etag) {
-                        header('HTTP/1.1 304 Not Modified');
-                    } else {
-                        header('Content-length: '.bytes($user_data['contact_jpegphoto']));
-                        echo $user_data['contact_jpegphoto'];
-                    }
-                    exit();
-                } else {
-                    if ($image && is_string($image)) {
-                        header("Location: $image");
-                    } else {
-                        header('HTTP/1.0 404 Not Found');
-                        die();
-                    }
-                    exit;
-                }
-                ob_end_clean();
             }
+            exit;
+            ob_end_clean();
         }
         exit();
     }
